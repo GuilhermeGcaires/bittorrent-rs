622b5b65d3ffa2a458371bdf737ac18200617e26
guilhermegcaires <guilhermegcaires@gmail.com>
2025-04-27 19:47:46 -0300

    Bencode decode

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,12 @@ use std::env;
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    if let Some((len, content)) = encoded_value.split_once(':') {
+        if let Ok(len) = len.parse::<usize>() {
+            return serde_json::Value::String(content[..len].to_string());
+        }
     }
+    panic!("Unhandled encoded value: {}", encoded_value);
 }
 
 fn main() {
